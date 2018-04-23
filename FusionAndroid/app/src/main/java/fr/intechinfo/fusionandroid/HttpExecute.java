bddd45b6977d114dd7009457f5323911656b1a76
Hugo Loiseau <hloiseau@intechinfo.fr>
2018-04-23 09:16:14 +0200

    Safe executor for post exception

--- FusionAndroid/app/src/main/java/fr/intechinfo/fusionandroid/HttpExecute.java
+++ FusionAndroid/app/src/main/java/fr/intechinfo/fusionandroid/HttpExecute.java
@@ -1,16 +1,18 @@
 package fr.intechinfo.fusionandroid;
 
 import android.util.Log;
+
+
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class HttpExecute extends Thread {
     
-    Call _call;
+    CallSafeExecutor _call;
     
     public HttpExecute(Call call){
-        _call = call;
+        _call = new CallSafeExecutor(call);
     }
     
    public static RetrofitAPI BuildAPI(){
@@ -20,13 +22,9 @@ public class HttpExecute extends Thread {
                .build();
        return retrofit.create(RetrofitAPI.class);
    }
-   
-   
+
     public void run(){
-        try {
-            Log.d("post", _call.execute().message());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Log.d("post", _call.execute().message());
     }
 }
+
