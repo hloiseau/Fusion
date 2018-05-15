e10fab64c65878f59449de40f3313701e315088b
Hugo Loiseau <hloiseau@intechinfo.fr>
2018-05-15 15:43:06 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- FusionAndroid/app/src/main/java/fr/intechinfo/fusionandroid/SmsBroadcastReceiver.java
+++ FusionAndroid/app/src/main/java/fr/intechinfo/fusionandroid/SmsBroadcastReceiver.java
@@ -3,6 +3,7 @@ package fr.intechinfo.fusionandroid;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Telephony;
@@ -22,6 +23,7 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "SMSBroadcastReceiver.onReceive(Context, Intent)");
         if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
             String smsSender;
             String smsBody = "";
@@ -31,8 +33,13 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                 smsSender = smsMessage.getDisplayOriginatingAddress();
                 smsBody += smsMessage.getMessageBody();
                 smsDate = smsMessage.getTimestampMillis();
-                smsList.add(new SMS(smsBody, smsSender, String.valueOf(smsDate), "1"));
-            }
+                smsList.add(new SMS(smsSender, smsBody, String.valueOf(smsDate), "1"));
+                StringBuilder sb = new StringBuilder();
+                sb.append("Sender : " +  smsSender + "\n");
+                sb.append("Body : " + smsBody + "\n");
+                sb.append("Date : " + smsDate + "\n");
+                Log.d(TAG, sb.toString());
+        }
             SMSList list = new SMSList();
             list.SetSMS(smsList);
             RetrofitAPI retrofitAPI = HttpExecute.BuildAPI();
